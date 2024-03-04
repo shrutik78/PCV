c60b7cb4b9272819851175ef4cd35939e1b3f61c
shruti <shruti1.mobicloud@gmail.com>
2024-03-04 12:31:23 +0530

    change service file

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -11,152 +11,152 @@ apiUrl='http://192.168.29.78:8000/api/'
 
   constructor(private http:HttpClient) { }
 
-  // getFRWheelImages(){
-  
-  
-  //   return this.http.get(this.baseUrl + 'wheelimages/')
-
-  // }
-
-  // getFRRoofImages(){
-
-  //   return this.http.get(this.baseUrl + 'roofimages/')
-
-  // }
-
-  // getFRMirrorImages(){
-  //     return this.http.get(this.baseUrl + 'outsidemirrorimages/')
-
-  // }
-
-  // getFLCapImages(){
-
-  //   return this.http.get(this.baseUrl + 'hubcapimages/')
-
-  // }
-
-  // getFLDoorImages(){
-
-  //   return this.http.get(this.baseUrl + 'doorimages/')
-
-  // }
-
-  // getFLHandleImages(){
-  
-  //   return this.http.get(this.baseUrl  + 'doorhandleimages/')
-
-  // }
-
-  // getFLWindowImages(){
-
-  //   return this.http.get(this.baseUrl  + 'windowimages/')
-
-  // }
-
-  // getRRLogoImages(){
- 
-  //   return this.http.get(this.baseUrl + 'logoimages/')
-
-  // }
-
-  // getRRHoodImages(){
- 
-  //   return this.http.get(this.baseUrl + 'hoodimages/')
-
-  // }
-
-  // getRRWiperImages(){
-
-  //   return this.http.get(this.baseUrl + 'wiperimages/')
-
-  
-  // }
-
-  // getRLTrunkImages(){
-   
-  //   return this.http.get(this.baseUrl + 'trunkimages/')
-
-  // }
-
-  // getRLLightImages(){
-   
-  //   return this.http.get(this.baseUrl  + 'taillightimages/')
-
-  // }
-
-
   getFRWheelImages(){
   
   
-    return this.http.get(this.apiUrl + 'wheelimages/')
+    return this.http.get(this.baseUrl + 'wheelimages/')
 
   }
 
   getFRRoofImages(){
 
-    return this.http.get(this.apiUrl + 'roofimages/')
+    return this.http.get(this.baseUrl + 'roofimages/')
 
   }
 
   getFRMirrorImages(){
-      return this.http.get(this.apiUrl + 'outsidemirrorimages/')
+      return this.http.get(this.baseUrl + 'outsidemirrorimages/')
 
   }
 
   getFLCapImages(){
 
-    return this.http.get(this.apiUrl + 'hubcapimages/')
+    return this.http.get(this.baseUrl + 'hubcapimages/')
 
   }
 
   getFLDoorImages(){
 
-    return this.http.get(this.apiUrl + 'doorimages/')
+    return this.http.get(this.baseUrl + 'doorimages/')
 
   }
 
   getFLHandleImages(){
   
-    return this.http.get(this.apiUrl  + 'doorhandleimages/')
+    return this.http.get(this.baseUrl  + 'doorhandleimages/')
 
   }
 
   getFLWindowImages(){
 
-    return this.http.get(this.apiUrl  + 'windowimages/')
+    return this.http.get(this.baseUrl  + 'windowimages/')
 
   }
 
   getRRLogoImages(){
  
-    return this.http.get(this.apiUrl + 'logoimages/')
+    return this.http.get(this.baseUrl + 'logoimages/')
 
   }
 
   getRRHoodImages(){
  
-    return this.http.get(this.apiUrl + 'hoodimages/')
+    return this.http.get(this.baseUrl + 'hoodimages/')
 
   }
 
   getRRWiperImages(){
 
-    return this.http.get(this.apiUrl + 'wiperimages/')
+    return this.http.get(this.baseUrl + 'wiperimages/')
 
   
   }
 
   getRLTrunkImages(){
    
-    return this.http.get(this.apiUrl + 'trunkimages/')
+    return this.http.get(this.baseUrl + 'trunkimages/')
 
   }
 
   getRLLightImages(){
    
-    return this.http.get(this.apiUrl  + 'taillightimages/')
+    return this.http.get(this.baseUrl  + 'taillightimages/')
 
   }
+
+
+  // getFRWheelImages(){
+  
+  
+  //   return this.http.get(this.apiUrl + 'wheelimages/')
+
+  // }
+
+  // getFRRoofImages(){
+
+  //   return this.http.get(this.apiUrl + 'roofimages/')
+
+  // }
+
+  // getFRMirrorImages(){
+  //     return this.http.get(this.apiUrl + 'outsidemirrorimages/')
+
+  // }
+
+  // getFLCapImages(){
+
+  //   return this.http.get(this.apiUrl + 'hubcapimages/')
+
+  // }
+
+  // getFLDoorImages(){
+
+  //   return this.http.get(this.apiUrl + 'doorimages/')
+
+  // }
+
+  // getFLHandleImages(){
+  
+  //   return this.http.get(this.apiUrl  + 'doorhandleimages/')
+
+  // }
+
+  // getFLWindowImages(){
+
+  //   return this.http.get(this.apiUrl  + 'windowimages/')
+
+  // }
+
+  // getRRLogoImages(){
+ 
+  //   return this.http.get(this.apiUrl + 'logoimages/')
+
+  // }
+
+  // getRRHoodImages(){
+ 
+  //   return this.http.get(this.apiUrl + 'hoodimages/')
+
+  // }
+
+  // getRRWiperImages(){
+
+  //   return this.http.get(this.apiUrl + 'wiperimages/')
+
+  
+  // }
+
+  // getRLTrunkImages(){
+   
+  //   return this.http.get(this.apiUrl + 'trunkimages/')
+
+  // }
+
+  // getRLLightImages(){
+   
+  //   return this.http.get(this.apiUrl  + 'taillightimages/')
+
+  // }
 
   getAllImages(vinNumber: string, images: any[]): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
